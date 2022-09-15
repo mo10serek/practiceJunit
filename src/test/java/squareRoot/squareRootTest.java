7593365c84dc78426bc1c4902b0fc171c8603f0d
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-09-15 10:52:31 -0400

    writing a 2nd procedure

--- src/test/java/squareRoot/squareRootTest.java
+++ src/test/java/squareRoot/squareRootTest.java
@@ -3,6 +3,9 @@ package squareRoot;
 import junit.framework.TestCase;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Types;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,7 +13,13 @@ public class squareRootTest extends TestCase {
 
     squareRoot squareRoot = new squareRoot();
 
+    @Test
     public void testSquareRoot() {
         assertEquals(squareRoot.squareRoot(4), 2.0);
+    }
+
+    @Test
+    public void CheckifTypeIsDouble(){
+        assertTrue(Double.class.isInstance(squareRoot.squareRoot(16)));
     }
 }
