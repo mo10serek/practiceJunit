554b42f968d5af516e751ec9ae66315b00880b26
MichaelBalcerzak <balcerzak.michael@gmail.com>
2022-09-14 21:25:41 -0400

    writing the test case

---
+++ src/main/java/squareRoot/squareRoot.java
@@ -0,0 +1,9 @@
+package squareRoot;
+
+public class squareRoot {
+
+    public double squareRoot(double x){
+
+        return 0;
+    }
+}
---
+++ src/test/java/squareRoot/squareRootTest.java
@@ -0,0 +1,16 @@
+package squareRoot;
+
+import junit.framework.TestCase;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class squareRootTest extends TestCase {
+
+    squareRoot squareRoot = new squareRoot();
+
+    public void testSquareRoot() {
+        assertEquals(squareRoot.squareRoot(4), 2);
+    }
+}
